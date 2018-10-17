dcab1c32268d2a0dfd3c110a9eb686acba6f2563
0x00-pl <0x00.pl@gmail.com>
2018-10-17 15:13:24 +0800

    remove unused file

--- main.py
+++ main.py
@@ -1,45 +1,72 @@
-
 import tensorflow as tf
+import numpy as np
+from PIL import Image
+
+from tensorflow.python.platform import gfile
+
+import tensor_head_to_tensor_list
+import tensor_list_to_layers
+import layer_list_to_k210_layer
+import k210_layer_to_c_code
+
+
+def load_graph(pb_file_path, tensor_head_name):
+    with tf.Session() as persisted_sess:
+        print("load graph")
+        with gfile.FastGFile(pb_file_path, 'rb') as f:
+            graph_def = tf.GraphDef()
+            graph_def.ParseFromString(f.read())
+            persisted_sess.graph.as_default()
+            tf.import_graph_def(graph_def, name='')
+
+
+        writer = tf.summary.FileWriter("./graphs", persisted_sess.graph)
+        writer.close()
+
+        return persisted_sess.graph._nodes_by_name[tensor_head_name].outputs[0]
+
+
+def box_image(im_path, new_h, new_w):
+    orig = Image.open(im_path)
+    w, h = orig.size
+    w_scale = float(new_w) / w
+    h_scale = float(new_h) / h
+
+    n_w = new_w
+    n_h = new_h
+    if w_scale < h_scale:
+        n_h = int(h * w_scale)
+    else:
+        n_w = int(w * h_scale)
+
+    resized = np.array(orig.resize([n_w, n_h]), dtype='float32') / 255.0
+
+    box_im = np.ones([new_h, new_w, 3], dtype='float32') * 0.5
+    fill_y = (new_h - n_h) >> 1
+    fill_x = (new_w - n_w) >> 1
+    box_im[fill_y:fill_y + n_h, fill_x:fill_x + n_w, :] = resized
+
+    return box_im, resized
 
 
 def main():
-    # Build your graph.
+    t = load_graph("graph_yv2_DW.pb", 'yv2')
+
     with tf.Session() as sess:
-        x = tf.constant([[37.0, -23.0], [1.0, 4.0]])
-        w = tf.Variable(tf.ones([2, 2]))
-        y = tf.matmul(x, w)
-        # ...
-        sess.run(tf.global_variables_initializer())
-        print(sess.run(x))
-        print(sess.run(w))
-        print(sess.run(y))
+        converter = tensor_head_to_tensor_list.PbConverter(t)
+        converter.convert()
+        layers = tensor_list_to_layers.convert_to_layers(sess, converter.dst)
+        dataset = np.array([box_image(path, 240, 320)[0].tolist() for path in
+                            # ('pic/001.jpg', 'pic/002.jpg', 'pic/003.jpg', 'pic/004.jpg', 'pic/005.jpg', 'pic/006.jpg')
+                            ('pic/dog.bmp', )
+                            ])
+        k210_layers = layer_list_to_k210_layer.gen_k210_layers(layers, sess, {'input:0': dataset})
 
-        loss = tf.reduce_sum(tf.square(tf.subtract(y, [[1, 0], [0, 1]])))
-        train_op = tf.train.AdamOptimizer(0.01).minimize(loss)
+        code = k210_layer_to_c_code.gen_layer_list_code(k210_layers)
+        with open('gencode_output.c', 'w') as of:
+            of.write(code)
 
-        # `sess.graph` provides access to the graph used in a <a href="../api_docs/python/tf/Session"><code>tf.Session</code></a>.
-        writer = tf.summary.FileWriter("/tmp/log/bbbb", sess.graph)
-
-
-        # Perform your computation...
-        for i in range(10000):
-            sess.run(train_op)
-            if i % 1000 == 0:
-                print(sess.run(loss))
-
-        print(sess.run(x))
-        print(sess.run(w))
-        print(sess.run(tf.matmul(x, w)))
-        writer.close()
-
-        print(sess.graph.get_all_collection_keys())
-        vars = sess.graph.get_collection('variables')
         pass
-
-
-        # builder = tf.saved_model.builder.SavedModelBuilder('/tmp/log/bbbb/savedbuilder')
-        # builder.add_meta_graph_and_variables(sess, ['tag_a'])
-        # builder.save(True)
 
 
 main()
