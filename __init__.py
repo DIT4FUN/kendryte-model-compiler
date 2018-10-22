3b9428f5022ac7bf75e77e53eacdec6d2971892b
0x00-pl <0x00.pl@gmail.com>
2018-10-22 16:25:33 +0800

    add argparse

--- __init__.py
+++ __init__.py
@@ -1,3 +1,5 @@
+import argparse
+
 import tensorflow as tf
 import numpy as np
 from PIL import Image
@@ -26,7 +28,7 @@ def load_graph(pb_file_path, tensor_head_name):
         return persisted_sess.graph._nodes_by_name[tensor_head_name].outputs[0]
 
 
-def box_image(im_path, new_h, new_w):
+def box_image(im_path, new_w, new_h):
     orig = Image.open(im_path)
     w, h = orig.size
     w_scale = float(new_w) / w
@@ -60,16 +62,36 @@ def convert(tensor_head, dataset_pack, eight_bit_mode=False):
 
 
 def main():
-    tensor_head = load_graph("graph_yv2_DW.pb", 'yv2')
-    eight_bit_mode = False
-    dataset = np.array([box_image(path, 240, 320)[0].tolist() for path in
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--pb_path', nargs=1, default='graph_yv2_DW.pb')
+    parser.add_argument('--tensor_head_name', nargs=1, default='yv2')
+    parser.add_argument('--dataset_input_name', nargs=1, default='input:0')
+    parser.add_argument('--dataset_pic_path', nargs=1, default='pic/dog.bmp')
+    parser.add_argument('--image_w', nargs=1, type=int, default=320)
+    parser.add_argument('--image_h', nargs=1, type=int, default=240)
+    parser.add_argument('--eight_bit_mode', nargs=1, type=bool, default=False)
+    parser.add_argument('--output_path', nargs=1, default='gencode_output.c')
+    args = parser.parse_args()
+
+    pb_path = args.pb_path
+    tensor_head_name = args.tensor_head_name
+    dataset_input_name = args.dataset_input_name
+    dataset_pic_path = args.dataset_pic_path
+    image_w = args.image_w
+    image_h = args.image_h
+    eight_bit_mode = args.eight_bit_mode
+    output_path = args.output_path
+
+
+    tensor_head = load_graph(pb_path, tensor_head_name)
+    dataset = np.array([box_image(path, image_w, image_h)[0].tolist() for path in
                         # ('pic/001.jpg', 'pic/002.jpg', 'pic/003.jpg', 'pic/004.jpg', 'pic/005.jpg', 'pic/006.jpg')
-                        ('pic/dog.bmp', )
+                        (dataset_pic_path, )
                         ])
 
-    code = convert(tensor_head, {'input:0': dataset}, eight_bit_mode)
+    code = convert(tensor_head, {dataset_input_name: dataset}, eight_bit_mode)
 
-    with open('gencode_output.c', 'w') as of:
+    with open(output_path, 'w') as of:
             of.write(code)
 
 main()
